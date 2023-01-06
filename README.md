0f76818c10cbbfe16eabb005e26906c66a73b692
Burkhard Heisen <burkhard.heisen@heisenware.com>
2023-01-06 15:42:52 +0100

    Preparations for publishing

--- README.md
+++ README.md
@@ -1,9 +1,28 @@
-# luxafor-agent
-Provides access to luxafor devices
+# Luxafor Agent
 
+Provides access to luxafor devices
 
 ## Starting instructions
 
+The Luxafor agent must have the rights to access the USB port.
+
+Under Linux you can achieve that by starting the application using `sudo`.
+Note that you have to provide the absolute path to the executable in that case.
+
+Example:
+
 ```bash
-sudo /home/bheisen/.nvm/versions/node/v16.15.1/bin/node index.js -a 'Luxafor Agent' -d demo-burkhard -t Kt1QKBwASNwMnlGQtq_3B -b mqtts://heisenware.rocks --bestEffort
+sudo /home/bheisen/.nvm/versions/node/v16.15.1/bin/node index.js -a 'Luxafor Agent' -d <domain>
 ```
+
+You can then try out your device using [vrpc.io](https://live.vrpc.io)
+
+## Integration to Heisenware's App Builder
+
+Example:
+
+```bash
+sudo /home/bheisen/.nvm/versions/node/v16.15.1/bin/node index.js -a 'Luxafor Agent' -d <domain> -t <token> -b mqtts://heisenware.rocks --bestEffort
+```
+
+You will then see the device directly in the Connectivity list.
---
+++ src/Luxafor.js
@@ -0,0 +1,141 @@
+const os = require('os')
+const HID = require('node-hid')
+const parseColor = require('parse-color')
+
+const COMMAND = {
+  color: 0x01,
+  fadeTo: 0x02,
+  flash: 0x03,
+  wave: 0x04,
+  police: 0x06,
+  rainbow: 0x06
+}
+
+const TARGET = {
+  all: 0xff,
+  top: 0x41,
+  bottom: 0x42,
+  one: 0x01,
+  two: 0x02,
+  three: 0x03,
+  four: 0x04,
+  five: 0x05,
+  six: 0x06
+}
+
+class Luxafor {
+  static hasDevice () {
+    Luxafor._device = null
+    return !!Luxafor._getDevice()
+  }
+
+  static getDevices () {
+    return HID.getDevices()
+  }
+
+  /**
+   * Change color to given color
+   * @param {String} color The color to be set
+   * @param {String} [target='all'] Defines which LEDs to set (all, top, bottom, one, two, three, four, five, six)
+   */
+  static setColor (color, target = 'all') {
+    const targetHex = Luxafor._getTarget(target)
+    Luxafor._write(COMMAND.color, targetHex, color, [0x00, 0x00, 0x00])
+  }
+
+  /**
+   * Gradually change color with a given speed
+   * @param {String} color The color to be set
+   * @param {Number} [speed=20] The transition time
+   * @param {String} [target='all'] Defines which LEDs to set (all, top, bottom, one, two, three, four, five, six)
+   */
+  static fadeTo (color, speed = 20, target = 'all') {
+    const targetHex = Luxafor._getTarget(target)
+    Luxafor._write(COMMAND.fadeTo, targetHex, color, [speed, 0x00, 0x00])
+  }
+
+  /**
+   * Flash given color and return to previous state
+   * @param {String} color Color to flash to
+   * @param {Number} [speed=20] Flashing speed
+   * @param {Number} [repeat=5] Number of repetitions
+   * @param {String} [target='all'] Defines which LEDs to set (all, top, bottom, one, two, three, four, five, six)
+   */
+  static flash (color, speed = 20, repeat = 5, target = 'all') {
+    const targetHex = Luxafor._getTarget(target)
+    Luxafor._write(COMMAND.flash, targetHex, color, [speed, 0x00, repeat])
+  }
+
+  /**
+   * Start a police pattern
+   * @param {Number} [repeat=5] Number of repetitions
+   */
+  static police (repeat = 5) {
+    Luxafor._writeRaw([COMMAND.police, 0x05, repeat])
+  }
+
+  /**
+   * Start a rainbow pattern
+   * @param {Number} [repeat=5] Number of repetitions
+   */
+  static rainbow (repeat = 5) {
+    Luxafor._writeRaw([COMMAND.rainbow, 0x08, repeat])
+  }
+
+  /**
+   * Turn off all the LEDs
+   */
+  static off () {
+    Luxafor.setColor('#000')
+  }
+
+  static _getTarget (target) {
+    const targetHex =
+      typeof target === 'number' && number < 7 ? target : TARGET[target]
+    if (!targetHex) throw new Error(`Invalid target: ${target}`)
+    return targetHex
+  }
+
+  static _write (command, target, color, options = []) {
+    const [r, g, b] = parseColor(color).rgb
+    const data = [command, target, r, g, b, ...options]
+    console.log('data', data)
+    Luxafor._writeRaw(data)
+  }
+
+  static _writeRaw (data) {
+    Luxafor._getDevice()
+    if (os.platform() === 'win32') {
+      data.unshift(0)
+    }
+
+    if (!Luxafor._device) {
+      throw new Error('No device available')
+    }
+    try {
+      Luxafor._device.resume()
+      Luxafor._device.write(data)
+      Luxafor._device.pause()
+    } catch (err) {
+      Luxafor._device = null
+      throw new Error(`Failed to write to device, because: ${err}`)
+    }
+  }
+
+  static getDevices () {
+    return HID.devices()
+  }
+
+  static _getDevice () {
+    if (!Luxafor._device) {
+      const path = HID.devices()
+        .filter(({ product }) => product.startsWith('LUXAFOR'))
+        .map(({ path }) => path)[0]
+      Luxafor._device = path ? new HID.HID(path) : null
+      if (Luxafor._device) Luxafor._device.pause()
+    }
+    return Luxafor._device
+  }
+}
+
+module.exports = Luxafor
